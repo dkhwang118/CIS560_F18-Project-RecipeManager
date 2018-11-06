e5421b1422cdeb9be8f3dba3a4c39b64e8b6431f
Laurel Thomson <laurel228@ksu.edu>
2018-11-06 16:32:08 -0600

    Added folders to group Recipe, Pantry, and Shopping List files together

---
+++ CIS560-RecipeManager/CIS560-RecipeManager/RecipeManager/uiAddNewRecipe_BASIC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS560_RecipeManager
+{
+    public partial class uiAddNewRecipe_BASIC : Form
+    {
+        public uiAddNewRecipe_BASIC()
+        {
+            InitializeComponent();
+        }
+
+        public void uxButton_AddRecipe_Click(object sender, EventArgs e)
+        {
+            RecipeRepository rrTemp = new RecipeRepository();
+            Recipe temp = rrTemp.CreateRecipe(uxTextBox_RecipeName.Text, uxTextBox_RecipeDescription.Text);
+            MessageBox.Show("Recipe " + temp.Name + " was created!");
+            
+        }
+    }
+}
---
+++ CIS560-RecipeManager/CIS560-RecipeManager/Repository/IQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS560_RecipeManager
+{
+    public interface IQuery
+    {
+        void AddRecipe(Recipe recipe);
+
+        void DeleteRecipe(Recipe recipe);
+
+        void AddIngredient(Ingredient ingredient);
+
+        ICollection<Recipe> GetAvailableRecipes();
+
+        ShoppingList GetShoppingList(ICollection<Recipe> recipes);
+
+        void UpdateIngredientQuantity(int quantity, Ingredient ingredient);
+
+        void AddRecipeCategory(RecipeCategory category);
+
+        void DeleteRecipeCategory(RecipeCategory category);
+
+        void CookRecipe(Recipe recipe);
+    }
+}
