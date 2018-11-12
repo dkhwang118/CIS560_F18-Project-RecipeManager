c21531f152f7713b43ee6c6c3cec707c8434f6a9
Daniel Bramucci <dbramuccicode@gmail.com>
2018-11-11 23:10:09 -0600

    Added a stored procedure and the code to read an ingredient from the database. Added test data to the database.

---
+++ CIS560-RecipeManager/CIS560-RecipeManager/Sql/Procedures/dbo.FindPantryItemByID.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].FindPantryItemByID
+	@ItemID int
+AS
+	SELECT p.PantryItemID, 
+           p.PantryItemName,
+           p.QuantityInPantry,
+           p.ItemUnitMeasurement
+	FROM PantryItem p
+	WHERE p.PantryItemID = @ItemID 
+RETURN 0
