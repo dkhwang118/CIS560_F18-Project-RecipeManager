fa3ef310b742d56f25a9d3738ce194431ab0aea3
Daniel Bramucci <dbramuccicode@gmail.com>
2018-11-11 23:10:09 -0600

    Added a stored procedure and the code to read an ingredient from the database. Added test data to the database.

---
+++ CIS560-RecipeManager/CIS560-RecipeManager/RecipeDatabaseDataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace CIS560_RecipeManager
+{
+
+
+    partial class RecipeDatabaseDataSet
+    {
+        partial class PantryItemDataTable
+        {
+        }
+    }
+}
+
+namespace CIS560_RecipeManager.RecipeDatabaseDataSetTableAdapters {
+    
+    
+    public partial class PantryItemTableAdapter {
+    }
+}
