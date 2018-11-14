30027500e1e80066a71725f314ea48cc79bec717
dkhwang118 <dkhwang@ksu.edu>
2018-11-14 12:20:55 -0600

    Moved "CreateIngredient.cs" to its proper place among the other partial Query classes

---
+++ CIS560-RecipeManager/CIS560-RecipeManager/Repository/Query/CreateIngredient.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * CreateIngredient.cs
+ * partial Query class to implement CreateIngredient() method
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Transactions;
+using CIS560_RecipeManager.RecipeManager;
+
+namespace CIS560_RecipeManager.Repository
+{
+    public partial class Query : IQuery
+    {
+        public Ingredient CreateIngredient(string ingredientName, string unitOfMeasurement, int unitCount)
+        {
+            using (var connection = new SqlConnection(Properties.Settings.Default.RecipeDatabaseConnectionString))
+            {
+                using (var transaction = new TransactionScope())
+                {
+                    using (var command = new SqlCommand("dbo.AddIngredient", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("Name", ingredientName);
+                        command.Parameters.AddWithValue("QuantityInPantry", unitCount);
+                        command.Parameters.AddWithValue("UnitMeasurement", unitOfMeasurement);
+                        var param = command.Parameters.Add("IngredientId", SqlDbType.Int);
+                        param.Direction = ParameterDirection.Output;
+
+                        connection.Open();
+
+                        command.ExecuteNonQuery(); // no results coming back
+
+                        transaction.Complete();
+
+                        return new Ingredient((int)param.Value, ingredientName, unitOfMeasurement); // return an Ingredient object with the same data that has been written to the DB
+                    }
+                } // should close transaction here automatically
+            } // should close connection here automatically
+        }
+    }
+}
---
+++ CIS560-RecipeManager/CIS560-RecipeManager/Repository/Query/Query_TEST.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Query_TEST.cs
+ * partial Query class used for testing individual IQuery methods. Meant only to prevent errors upon compilation
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CIS560_RecipeManager.RecipeManager;
+
+namespace CIS560_RecipeManager.Repository
+{
+    public partial class Query : IQuery
+    {
+        public Recipe CreateRecipe(string recipeName, string recipeDescription, IDictionary<Ingredient, int> measuredIngredients)
+        {
+            // not implemented
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRecipe(Recipe recipe)
+        {
+            // not implemented
+            throw new NotImplementedException();
+        }
+
+
+        public IReadOnlyCollection<Recipe> GetAvailableRecipes()
+        {
+            // not implemented
+            throw new NotImplementedException();
+        }
+
+        public ShoppingList GetShoppingList(ICollection<Recipe> recipes)
+        {
+            // not implemented
+            throw new NotImplementedException();
+        }
+
+        public void UpdateIngredientQuantity(int quantity, Ingredient ingredient)
+        {
+            // not implemented
+            throw new NotImplementedException();
+        }
+
+        public void AddRecipeCategory(RecipeCategory category)
+        {
+            // not implemented
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRecipeCategory(RecipeCategory category)
+        {
+            // not implemented
+            throw new NotImplementedException();
+        }
+
+        public void CookRecipe(Recipe recipe)
+        {
+            // not implemented
+            throw new NotImplementedException();
+        }
+    }
+}
