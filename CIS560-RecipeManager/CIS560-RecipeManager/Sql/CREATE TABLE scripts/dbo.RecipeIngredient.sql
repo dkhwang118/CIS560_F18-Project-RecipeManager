e8113a821c9d963f04e86e0f5d952ed463a75426
David Hwang <dkhwang@ksu.edu>
2018-11-27 19:46:50 -0600

    Updated the RecipeCategory, RecipeIngredient, and ShoppingList Tables to correctly assign their default Id values. Sorry everyone! -DKH

--- CIS560-RecipeManager/CIS560-RecipeManager/Sql/CREATE TABLE scripts/dbo.RecipeIngredient.sql
+++ CIS560-RecipeManager/CIS560-RecipeManager/Sql/CREATE TABLE scripts/dbo.RecipeIngredient.sql
@@ -1,16 +1,9 @@
-﻿CREATE TABLE [dbo].[RecipeIngredient]
-(
-	[PantryItemID] INT NOT NULL, 
-    [RecipeID] INT NOT NULL, 
-    [RecipeQuantity] INT NOT NULL
+﻿CREATE TABLE [dbo].[RecipeIngredient] (
+    [PantryItemID]   INT IDENTITY(1, 1) NOT NULL,
+    [RecipeID]       INT NOT NULL,
+    [RecipeQuantity] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([PantryItemID] ASC, [RecipeID] ASC),
+    CONSTRAINT [FK_RecipeIngredient_Recipes] FOREIGN KEY ([RecipeID]) REFERENCES [dbo].[Recipes] ([RecipeID]),
+    CONSTRAINT [FK_RecipeIngredient_PantryItem] FOREIGN KEY ([PantryItemID]) REFERENCES [dbo].[PantryItem] ([PantryItemID])
+);
 
-	PRIMARY KEY(PantryItemID, RecipeID),
-
-	CONSTRAINT FK_RecipeIngredient_PantryItem FOREIGN KEY(PantryItemID)
-	REFERENCES [dbo].PantryItem(PantryItemID),
-
-	CONSTRAINT FK_RecipeIngredient_Recipes FOREIGN KEY(RecipeID)
-	REFERENCES [dbo].Recipes(RecipeID)
-
-
-)
