5d71d7d2da9681b9bf0f3dfc7fd76cb03f2cf00e
Laurel Thomson <laurel228@ksu.edu>
2018-11-28 08:20:04 -0600

    Overriding equals operator for Ingredient so that two Ingredients with the same Id are the same

--- CIS560-RecipeManager/CIS560-RecipeManager/PantryManager/Ingredient.cs
+++ CIS560-RecipeManager/CIS560-RecipeManager/PantryManager/Ingredient.cs
@@ -1,6 +1,8 @@
-﻿namespace CIS560_RecipeManager
+﻿using System;
+
+namespace CIS560_RecipeManager
 {
-    public class Ingredient
+    public class Ingredient : IEquatable<Ingredient>
     {
         public int Id { get; }
 
@@ -18,6 +20,37 @@
         public override string ToString()
         {
             return $"Ingredient(id: {Id}, name: {Name}, unit: {Unit})";
+        }
+
+        public bool Equals(Ingredient other)
+        {
+            return other.Id == this.Id;
+        }
+
+        public static bool operator ==(Ingredient ing1, Ingredient ing2)
+        {
+            return ing1.Equals(ing2);
+        }
+
+        public static bool operator !=(Ingredient ing1, Ingredient ing2)
+        {
+            return !ing1.Equals(ing2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == GetType() && Equals((Ingredient)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = Id.GetHashCode();
+                hashCode = (hashCode * 397) ^ Name.GetHashCode();
+                hashCode = (hashCode * 397) ^ Unit.GetHashCode();
+                return hashCode;
+            }
         }
     }
 }
