fb6fed4c602f7f109f1ee36d2fb8572cdaeba38d
David Hwang <dkhwang@ksu.edu>
2018-11-28 16:20:31 -0600

    Quick commit to see what is wrong on other branch

---
+++ CIS560-RecipeManager/CIS560-RecipeManager/Sql/Procedures/dbo.GetAffordableRecipes.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetAffordableRecipes]
+	@MaxMoneyToSpendInCents INT
+
+AS
+SELECT DISTINCT tempCTE.RecipeID, tempCTE.RecipeCostInCents
+FROM 
+( 
+	SELECT r.RecipeID, 
+		(SUM(pItem.UnitPriceInCents * (rIng.RecipeQuantity - pItem.QuantityInPantry)) OVER(PARTITION BY r.RecipeID)) RecipeCostInCents
+	FROM	[dbo].Recipes r
+	INNER JOIN [dbo].RecipeIngredient rIng ON r.RecipeID = rIng.RecipeID
+	INNER JOIN [dbo].PantryItem pItem ON rIng.PantryItemID = pItem.PantryItemID
+	) tempCTE
+WHERE RecipeCostInCents <= @MaxMoneyToSpendInCents 
