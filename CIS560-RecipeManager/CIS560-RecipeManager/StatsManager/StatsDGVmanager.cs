3eb48b7bebd3cd41e83ac1232a72c6bec2b1b48a
dkhwang118 <dkhwang@ksu.edu>
2018-11-30 10:36:36 -0600

    wrote more out. Still building skeleton of Form flow

---
+++ CIS560-RecipeManager/CIS560-RecipeManager/StatsManager/StatsDGVmanager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using CIS560_RecipeManager.RecipeManager;
+
+namespace CIS560_RecipeManager.StatsManager
+{
+    public class StatsDGVmanager
+    {
+        private IQuery _query;
+        private ICollection<Recipe> _totalRecipes;
+        private ICollection<RecipeCategory> _totalCategories;
+
+        public BindingList<Recipe> VisibleRecipes { get; }
+        public BindingList<RecipeCategory> VisibleRecipeCategories { get; }
+
+        public StatsDGVmanager(IQuery query)
+        {
+            _query = query;
+        }
+
+        private void PopulateDGV(int dataTypeParam)
+        {
+            // method to update the DGV with query information.
+        }
+
+    }
+}
--- CIS560-RecipeManager/CIS560-RecipeManager/StatsManager/uiStats.Designer.cs
+++ CIS560-RecipeManager/CIS560-RecipeManager/StatsManager/uiStats.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StatsDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.StatsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StatsDataGridView
+            // 
+            this.StatsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StatsDataGridView.Location = new System.Drawing.Point(441, 277);
+            this.StatsDataGridView.Name = "StatsDataGridView";
+            this.StatsDataGridView.RowTemplate.Height = 33;
+            this.StatsDataGridView.Size = new System.Drawing.Size(240, 150);
+            this.StatsDataGridView.TabIndex = 0;
+            // 
+            // uiStats
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "uiStats";
+            this.ClientSize = new System.Drawing.Size(971, 697);
+            this.Controls.Add(this.StatsDataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "uiStats";
+            this.Text = "Statistics and Information";
+            ((System.ComponentModel.ISupportInitialize)(this.StatsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView StatsDataGridView;
     }
 }
