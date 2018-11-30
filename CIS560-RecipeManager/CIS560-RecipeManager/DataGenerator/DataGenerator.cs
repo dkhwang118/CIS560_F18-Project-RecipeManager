08fc3c39174505d93b33eab7af5ca1cb7345b354
Daniel Bramucci <dbramuccicode@gmail.com>
2018-11-30 16:13:51 -0600

    Stubbed out Data Generator

---
+++ CIS560-RecipeManager/CIS560-RecipeManager/DataGenerator/DataGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CIS560_RecipeManager.DataGenerator
+{
+    static class DataGenerator
+    {
+
+        public static void GenerateAndInsertData()
+        {
+            throw new NotImplementedException("Data Generator not Implemented");
+        }
+
+        public static List<int> GenerateIngredients()
+        {
+            throw new NotImplementedException("Cannot generate ingredients yet");
+        }
+
+        public static List<int> GenerateRecipeCategories()
+        {
+            throw new NotImplementedException("Cannot make categories yet");
+        }
+
+        public static List<int> GenerateRecipe()
+        {
+            throw new NotImplementedException("I don't even know how recipes are made.");
+        }
+    }
+}
