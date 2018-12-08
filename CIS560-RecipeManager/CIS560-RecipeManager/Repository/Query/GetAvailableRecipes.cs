2ff2e8015ca749c34105c6c8aa355757a10e05fd
Daniel Bramucci <dbramuccicode@gmail.com>
2018-12-07 22:48:08 -0600

    Fixed that GetAvailableRecipes wasn't a stored procedure.

--- CIS560-RecipeManager/CIS560-RecipeManager/Repository/Query/GetAvailableRecipes.cs
+++ CIS560-RecipeManager/CIS560-RecipeManager/Repository/Query/GetAvailableRecipes.cs
@@ -26,24 +26,9 @@ namespace CIS560_RecipeManager.Repository
                     string description;
                     int categoryID;
 
-                    using (var command = new SqlCommand(@"
-SELECT R.RecipeID
-FROM[dbo].[Recipes] R
--- I would prefer for a way to check forall x.P(x) rather than not exists x.not p(x).
-WHERE NOT EXISTS
-   (
-      SELECT 1
-
-      FROM[dbo].RecipeIngredient RI
-
-        JOIN[dbo].PantryItem[PI] ON RI.PantryItemID = [PI].PantryItemID
-
-      WHERE R.RecipeID = RI.RecipeID
-
-        AND[PI].QuantityInPantry < RI.RecipeQuantity
-   )", connection))
+                    using (var command = new SqlCommand(@"[dbo].GetAvailableRecipes", connection))
                     {
-
+                        command.CommandType = CommandType.StoredProcedure;
                         var result = command.ExecuteReader();
                         
                         // I don't believe that checking for result.HasRows is needed in this use-case.
---
+++ CIS560-RecipeManager/CIS560-RecipeManager/SQL Code/Procedures/dbo.GetAvailableRecipes.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetAvailableRecipes]
+AS
+	SELECT R.RecipeID
+	FROM[dbo].[Recipes] R
+	-- I would prefer for a way to check forall x.P(x) rather than not exists x.not p(x).
+	WHERE NOT EXISTS
+	   (
+		  SELECT 1
+		  FROM[dbo].RecipeIngredient RI
+			JOIN[dbo].PantryItem[PI] ON RI.PantryItemID = [PI].PantryItemID
+		  WHERE R.RecipeID = RI.RecipeID
+			AND [PI].QuantityInPantry < RI.RecipeQuantity
+		);
+RETURN 0
